caa56557af857ba6058a02a3daa9ba27462e7373
punnam <punnamc@gmail.com>
2015-12-10 21:07:49 -0500

    push @ 9:07 pm

--- src/com/sdmtool/jetty/server/SDMToolAgent.java
+++ src/com/sdmtool/jetty/server/SDMToolAgent.java
@@ -1,55 +1,61 @@
 package com.sdmtool.jetty.server;
 
-//
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-//========================================================================
-//Copyright (c) 1995-2015 Mort Bay Consulting Pty. Ltd.
-//------------------------------------------------------------------------
-//All rights reserved. This program and the accompanying materials
-//are made available under the terms of the Eclipse Public License v1.0
-//and Apache License v2.0 which accompanies this distribution.
-//
-//  The Eclipse Public License is available at
-//  http://www.eclipse.org/legal/epl-v10.html
-//
-//  The Apache License v2.0 is available at
-//  http://www.opensource.org/licenses/apache2.0.php
-//
-//You may elect to redistribute this code under either of these licenses.
-//========================================================================
-//
-
-import java.io.IOException;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 public class SDMToolAgent {
 	public static void main(String[] args) throws Exception {
 		CommandMap commandMap = new CommandCli(args).parse();
 
-        setServerUrl(commandMap);
-      }
-  	private static String setServerUrl(CommandMap commandMap) throws IOException {
-  		   //String POST_URL ="http://localhost:8080/SDMToolAgent/rest/executeCommand/"; 
-  		   //String POST_URL =commandMap"http://localhost:8080/SDMToolAgent/rest/executeCommand/";
-  		
-  		   String POST_URL = "http://localhost:"+commandMap.getPortNum() + "/" + commandMap.getContextRoot()+ "/rest/setSDMToolServerUrl/";
-  		   System.out.println(POST_URL);
-  		   CloseableHttpClient client = HttpClients.createDefault();
-  		    HttpPost httpPost = new HttpPost(POST_URL);
-  		    StringEntity entity = new StringEntity(commandMap.getSdmServerUrl());
-  		    httpPost.setEntity(entity);
-  		    //httpPost.setHeader("Accept", "application/json");
-  		    //httpPost.setHeader("Content-type", "application/json");
-  		 
-  		    CloseableHttpResponse response = client.execute(httpPost);
-  		    System.out.println(response.getStatusLine().getStatusCode());
-  		    System.out.println(response.getStatusLine().getReasonPhrase());
-  		    client.close();
-  		    return "executed";
-      }
+		sendPost(commandMap);
+	}
+
+	private static void sendPost(CommandMap commandMap) throws Exception {
+
+		String POST_URL = "http://localhost:" + commandMap.getPortNum() + "/" + commandMap.getContextRoot();
+
+		HttpClient client = HttpClients.createDefault();
+		HttpPost post = new HttpPost();
+		URIBuilder builder = new URIBuilder(POST_URL+"/rest/setUrl/");
+		
+		post.setURI(builder.build());
+		//+ "/rest/setSDMToolServerUrl"
+		//InetAddress.getLocalHost().getHostAddress()
+		// add header
+		String USER_AGENT = "Mozilla/5.0";
+		post.setHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("SdmServerUrl", commandMap.getSdmServerUrl()));
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+		HttpResponse response = client.execute(post);
+		System.out.println("\nSending 'POST' request to URL : " + POST_URL);
+		System.out.println("Post parameters : " + post.getEntity());
+		System.out.println("Response Code : " + 
+                                    response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		System.out.println(result.toString());
+
+	}
 }
