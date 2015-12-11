44f68c0266853d7a6d03ec91c73ec993ab2c0526
punnam <punnamc@gmail.com>
2015-12-10 22:37:43 -0500

    push@10:30 pm

--- src/com/sdmtool/jetty/server/SDMToolAgent.java
+++ src/com/sdmtool/jetty/server/SDMToolAgent.java
@@ -1,7 +1,9 @@
 package com.sdmtool.jetty.server;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +15,49 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class SDMToolAgent {
 	public static void main(String[] args) throws Exception {
 		CommandMap commandMap = new CommandCli(args).parse();
-
-		sendPost(commandMap);
-	}
+        // Create a basic jetty server object that will listen on port 8080.
+        // Note that if you set this to port 0 then a randomly available port
+        // will be assigned that you can either look in the logs for the port,
+        // or programmatically obtain it for use in test cases.
+        Server server = new Server(commandMap.getPortNum());
+ 
+        // Setup JMX
+        MBeanContainer mbContainer = new MBeanContainer(
+                ManagementFactory.getPlatformMBeanServer());
+        server.addBean(mbContainer);
+ 
+        // The WebAppContext is the entity that controls the environment in
+        // which a web application lives and breathes. In this example the
+        // context path is being set to "/" so it is suitable for serving root
+        // context requests and then we see it setting the location of the war.
+        // A whole host of other configurations are available, ranging from
+        // configuring to support annotation scanning in the webapp (through
+        // PlusConfiguration) to choosing where the webapp will unpack itself.
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/"+commandMap.getContextRoot());
+        File warFile = new File(commandMap.getWarFile());
+        webapp.setWar(warFile.getAbsolutePath());
+ 
+        // A WebAppContext is a ContextHandler as well so it needs to be set to
+        // the server so it is aware of where to send the appropriate requests.
+        server.setHandler(webapp);
+ 
+        // Start things up! 
+        server.start();
+ 
+        // The use of server.join() the will make the current thread join and
+        // wait until the server is done executing.
+        // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+        server.join();
+        sendPost(commandMap);
+    }
 
 	private static void sendPost(CommandMap commandMap) throws Exception {
 
